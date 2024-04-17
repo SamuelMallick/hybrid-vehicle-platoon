eb721cebbf908175303665d93b20509bab720870
SamuelMallick <sam.mallick.97@gmail.com>
2024-04-17 16:16:35 +0200

    format cent for tests

--- fleet_cent_mld.py
+++ fleet_cent_mld.py
@@ -11,10 +11,6 @@ from scipy.linalg import block_diag
 
 from env import PlatoonEnv
 from misc.common_controller_params import Params, Sim
-from misc.leader_trajectory import (
-    ConstantVelocityLeaderTrajectory,
-    StopAndGoLeaderTrajectory,
-)
 from misc.spacing_policy import ConstantSpacingPolicy, SpacingPolicy, ConstantTimePolicy
 from models import Platoon
 from mpcs.cent_mld import MpcMldCent
@@ -24,18 +20,6 @@ from mpcs.mpc_gear import MpcGear, MpcNonlinearGear
 from plot_fleet import plot_fleet
 
 np.random.seed(2)
-
-PLOT = True
-SAVE = False
-
-n = Sim.n  # num cars
-N = Sim.N  # controller horizon
-ep_len = Sim.ep_len  # length of episode (sim len)
-ts = Params.ts
-
-spacing_policy= Sim.spacing_policy
-leader_trajectory = Sim.leader_trajectory
-leader_x = leader_trajectory.get_leader_trajectory()
 
 
 class MpcGearCent(MpcMldCent, MpcMldCentDecup, MpcGear):
@@ -72,7 +56,11 @@ class MpcNonlinearGearCent(MpcMldCent, MpcNonlinearGear):
 
 
 class TrackingCentralizedAgent(MldAgent):
-    def __init__(self, mpc: MpcMld) -> None:
+    def __init__(self, mpc: MpcMld, ep_len: int, N: int, leader_x: np.ndarray) -> None:
+        self.ep_len = ep_len
+        self.N = N
+        self.leader_x = leader_x
+
         self.solve_times = np.zeros((ep_len, 1))
         self.node_counts = np.zeros((ep_len, 1))
         self.bin_var_counts = np.zeros((ep_len, 1))
@@ -80,77 +68,91 @@ class TrackingCentralizedAgent(MldAgent):
 
     def on_timestep_end(self, env: Env, episode: int, timestep: int) -> None:
         # time step starts from 1, so this will set the cost accurately for the next time-step
-        self.mpc.set_leader_traj(leader_x[:, timestep : (timestep + N + 1)])
+        self.mpc.set_leader_traj(self.leader_x[:, timestep : (timestep + self.N + 1)])
         self.solve_times[env.step_counter - 1, :] = self.run_time
         self.node_counts[env.step_counter - 1, :] = self.node_count
         self.bin_var_counts[env.step_counter - 1, :] = self.num_bin_vars
         return super().on_timestep_end(env, episode, timestep)
 
     def on_episode_start(self, env: Env, episode: int, state) -> None:
-        self.mpc.set_leader_traj(leader_x[:, 0 : N + 1])
+        self.mpc.set_leader_traj(self.leader_x[:, 0 : self.N + 1])
         return super().on_episode_start(env, episode, state)
 
 
-# vehicles
-platoon = Platoon(n, vehicle_type=Sim.vehicle_model_type, masses=[500, 1500, 800, 1000])
-systems = platoon.get_vehicle_system_dicts(ts)
+def simulate(sim: Sim, save: bool = False, plot: bool = True):
+    n = Sim.n  # num cars
+    N = Sim.N  # controller horizon
+    ep_len = Sim.ep_len  # length of episode (sim len)
+    ts = Params.ts
 
-# env
-env = MonitorEpisodes(
-    TimeLimit(
-        PlatoonEnv(
-            n=n,
-            platoon=platoon,
-            leader_trajectory=leader_trajectory,
-            spacing_policy=spacing_policy,
-            start_from_platoon=Sim.start_from_platoon,
-        ),
-        max_episode_steps=ep_len,
+    spacing_policy = Sim.spacing_policy
+    leader_trajectory = Sim.leader_trajectory
+    leader_x = leader_trajectory.get_leader_trajectory()
+    # vehicles
+    platoon = Platoon(
+        n, vehicle_type=Sim.vehicle_model_type
     )
-)
+    systems = platoon.get_vehicle_system_dicts(ts)
 
-# mpcs
-if Sim.vehicle_model_type == "pwa_gear":
-    mpc = MpcMldCent(n, N, systems, spacing_policy=spacing_policy)
-elif Sim.vehicle_model_type == "pwa_friction":
-    mpc = MpcGearCent(n, N, systems, spacing_policy=spacing_policy)
-elif Sim.vehicle_model_type == "nonlinear":
-    mpc = MpcNonlinearGearCent(n, N, systems, spacing_policy=spacing_policy)
-else:
-    raise ValueError(f"{Sim.vehicle_model_type} is not a valid vehicle model type.")
+    # env
+    env = MonitorEpisodes(
+        TimeLimit(
+            PlatoonEnv(
+                n=n,
+                platoon=platoon,
+                leader_trajectory=leader_trajectory,
+                spacing_policy=spacing_policy,
+                start_from_platoon=Sim.start_from_platoon,
+            ),
+            max_episode_steps=ep_len,
+        )
+    )
+
+    # mpcs
+    if Sim.vehicle_model_type == "pwa_gear":
+        mpc = MpcMldCent(n, N, systems, spacing_policy=spacing_policy)
+    elif Sim.vehicle_model_type == "pwa_friction":
+        mpc = MpcGearCent(n, N, systems, spacing_policy=spacing_policy)
+    elif Sim.vehicle_model_type == "nonlinear":
+        mpc = MpcNonlinearGearCent(n, N, systems, spacing_policy=spacing_policy)
+    else:
+        raise ValueError(f"{Sim.vehicle_model_type} is not a valid vehicle model type.")
+
+    # agent
+    agent = TrackingCentralizedAgent(mpc, ep_len, N, leader_x)
+
+    agent.evaluate(env=env, episodes=1, seed=1)
+
+    if len(env.observations) > 0:
+        X = env.observations[0].squeeze()
+        U = env.actions[0].squeeze()
+        R = env.rewards[0]
+    else:
+        X = np.squeeze(env.ep_observations)
+        U = np.squeeze(env.ep_actions)
+        R = np.squeeze(env.ep_rewards)
+
+    print(f"Return = {sum(R.squeeze())}")
+    print(f"Violations = {env.unwrapped.viol_counter}")
+    print(f"Run_times_sum: {sum(agent.solve_times)}")
+    print(f"average_bin_vars: {sum(agent.bin_var_counts)/len(agent.bin_var_counts)}")
+
+    if plot:
+        plot_fleet(n, X, U, R, leader_x, violations=env.unwrapped.viol_counter[0])
+
+    if save:
+        with open(
+            f"cent_{Sim.id}" + ".pkl",
+            "wb",
+        ) as file:
+            pickle.dump(X, file)
+            pickle.dump(U, file)
+            pickle.dump(R, file)
+            pickle.dump(agent.solve_times, file)
+            pickle.dump(agent.node_counts, file)
+            pickle.dump(env.unwrapped.viol_counter[0], file)
+            pickle.dump(leader_x, file)
 
 
-# agent
-agent = TrackingCentralizedAgent(mpc)
-
-agent.evaluate(env=env, episodes=1, seed=1)
-
-if len(env.observations) > 0:
-    X = env.observations[0].squeeze()
-    U = env.actions[0].squeeze()
-    R = env.rewards[0]
-else:
-    X = np.squeeze(env.ep_observations)
-    U = np.squeeze(env.ep_actions)
-    R = np.squeeze(env.ep_rewards)
-
-print(f"Return = {sum(R.squeeze())}")
-print(f"Violations = {env.unwrapped.viol_counter}")
-print(f"Run_times_sum: {sum(agent.solve_times)}")
-print(f"average_bin_vars: {sum(agent.bin_var_counts)/len(agent.bin_var_counts)}")
-
-if PLOT:
-    plot_fleet(n, X, U, R, leader_x, violations=env.unwrapped.viol_counter[0])
-
-if SAVE:
-    with open(
-        f"cent" + ".pkl",
-        "wb",
-    ) as file:
-        pickle.dump(X, file)
-        pickle.dump(U, file)
-        pickle.dump(R, file)
-        pickle.dump(agent.solve_times, file)
-        pickle.dump(agent.node_counts, file)
-        pickle.dump(env.unwrapped.viol_counter[0], file)
-        pickle.dump(leader_x, file)
+if __name__ == "__main__":
+    simulate(Sim(), save=True)
--- misc/common_controller_params.py
+++ misc/common_controller_params.py
@@ -4,7 +4,15 @@ import numpy as np
 
 from misc.leader_trajectory import StopAndGoLeaderTrajectory
 from misc.spacing_policy import ConstantTimePolicy
-
+class Params:
+    Q_x = np.diag([1, 0.1])  # penalty of state tracking error
+    Q_u = 1 * np.eye(1)  # penalty on control effort
+    Q_du = 0 * np.eye(1)  # penalty on variation in control effort
+    w = 1e4  # penalty on slack variables
+    ts = 1
+    a_acc = 2.5  # acceleration limit
+    a_dec = -2  # deceleration limit
+    d_safe = 25
 
 class Sim:
     vehicle_model_type: Literal["nonlinear", "pwa_friction", "pwa_gear"] = "pwa_gear"
@@ -19,15 +27,8 @@ class Sim:
     # )
     spacing_policy = ConstantTimePolicy(10, 3)
     leader_trajectory = StopAndGoLeaderTrajectory(
-        p=3000, vh=30, vl=10, v_change_steps=[10, 50], trajectory_len=ep_len + 50, ts=ts
+        p=3000, vh=30, vl=10, v_change_steps=[10, 50], trajectory_len=ep_len + 50, ts=Params.ts
     )
+    id = "default"
 
-class Params:
-    Q_x = np.diag([1, 0.1])  # penalty of state tracking error
-    Q_u = 1 * np.eye(1)  # penalty on control effort
-    Q_du = 0 * np.eye(1)  # penalty on variation in control effort
-    w = 1e4  # penalty on slack variables
-    ts = 1  # time step that controller uses
-    a_acc = 2.5  # acceleration limit
-    a_dec = -2  # deceleration limit
-    d_safe = 25
+
